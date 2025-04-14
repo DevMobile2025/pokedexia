3d86bc5f1411490e4226941b0492da37ca2eeb14
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-04-14 15:15:11 -0300

    FEAT: Create fake db #5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { db } from "@/api/db.js";
+
+function createURL(src) {
+  const url = new URL(`../assets/images/` + src, import.meta.url).href;
+  return url;
+}
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <div v-for="(pokemon, id) in db.pokemons" :key="id">
+      <!-- {{ pokemon.image }} -->
+      <img :src="createURL(pokemon.image)" alt="">
+    </div>
+  </div>
 </template>
 
 <style scoped>
