a259a0dbabac4ac2733aee122e485ddb06dc3ca9
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-04-16 16:44:54 -0300

    FEAT: Create default routers #7

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
+      component: () => import('@/layouts/DefaultLayout.vue'),
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('@/views/HomeView.vue')
+        },
+        {
+          path: 'signup',
+          name: 'signUp',
+          component: () => import('@/views/SignUpView.vue')
+        },
+        {
+          path: 'cartshop',
+          name: 'cartShop',
+          component: () => import('@/views/CartShopView.vue')
+        }
+      ]
+    }
   ],
 })
 
-export default router
+export default router;
