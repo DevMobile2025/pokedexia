d7d1f8afe4e06dfe2b493b69f65939ca85a08f8a
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-04-21 20:23:12 -0300

    FEAT: Cart Shopp Screen #4

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,49 +1,41 @@
-import { reactive, ref } from 'vue';
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useCartStore = defineStore('cart', () => {
-    const state = reactive({
-        amountItems: 0
-    });
+    const productsInCart = ref([{id: 0, amount: 3}, {id: 1, amount: 1}]);
+
+    const amountItems = computed(() => productsInCart.value.length);
 
     const findIndexProduct = (productId) => {
-        return productsInCart.value.indexOf((product) => product.id == productId);
+        return productsInCart.value.findIndex((product) => product.id == productId);
     }
   
     const updateCard = (productId) => {
         const index = findIndexProduct(productId);
+        console.log(index);
         if (index != -1) {
-          productsInCart.value[index].amountItems++;
+          productsInCart.value[index].amount++;
         }
         else {
-            productsInCart.value.push({id: productId, amountItems: 1});
+            productsInCart.value.push({id: productId, amount: 1});
         }
     }
 
-    const removeOneQuantity = (productId) => {
-        const index = findIndexProduct(productId);
-
-        if (productsInCart.value[index].amountItems == 1) {
-            productsInCart.value.splice(index, 1);
-        } else {
-            productsInCart.value[index].amountItems--;
-        }
+    const removeOneQuantity = (index) => {
+        productsInCart.value[index].amount--;
     }
 
-    const removeItem = (productId) => {
-        const index = findIndexProduct(productId);
-
+    const removeItem = (index) => {
         productsInCart.value.splice(index, 1);
     }
     
-    const productsInCart = ref([{}]);
 
   return {
-    state,
+    productsInCart,
+    amountItems,
     findIndexProduct,
     updateCard,
     removeOneQuantity,
     removeItem,
-    productsInCart,
   }
 })
