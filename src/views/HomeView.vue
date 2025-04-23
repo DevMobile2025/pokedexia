99d223df83c907ae74af2eef5a23dd59861042a9
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2025-04-23 16:51:12 -0300

    feat: demarcação do header e do footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,13 +8,35 @@ function createURL(src) {
 </script>
 
 <template>
+  <header>
+    <div class="titulo"><h1>Pokédexia</h1></div>
+  </header>
   <div>
     <div v-for="(pokemon, id) in db.pokemons" :key="id">
       <!-- {{ pokemon.image }} -->
       <img :src="createURL(pokemon.image)" alt="">
     </div>
   </div>
+  <footer>ooiiiii</footer>
 </template>
 
 <style scoped>
+*{
+  max-width: 100%;
+}
+header{
+  width: 100vw;
+  height: 10vh;
+  background-color: red;
+}
+.titulo h1{
+  font-family: "Jockey One", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+ footer{
+  width: 100vw;
+  height: 10vh;
+  background-color: red;
+}
 </style>
