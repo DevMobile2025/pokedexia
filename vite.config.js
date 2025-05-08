ac31b8c2ce5ed74e8b477a1de319ca184c61ec20
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-05-08 14:39:01 -0300

    FEAT: Install PWA

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,48 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['pwa-180x180.png'],
+      manifest: {
+        name: 'Pokedexia',
+        short_name: 'pokedexia',
+        description: 'Pokedexia: um e-commerce de cartas Pokémon',
+        theme_color: '#ffffff',
+        start_url: 'https://pokedexia-suh0.onrender.com/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        id: 'com.pokedexia.app',
+        orientation: 'any',
+        background_color: '#ffffff',
+        start_url: '.',
+        launch_handler: {
+          client_mode: ['navigate-existing', 'auto'],
+        },
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
